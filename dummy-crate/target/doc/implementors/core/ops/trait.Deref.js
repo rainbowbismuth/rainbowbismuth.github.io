4f93005f1abe0321117010fbf13a9bf5cbd5535f
Emily A. Bellows <emily.a.bellows@gmail.com>
2016-02-17 23:03:01 -0500

    updated hamt-rs docs

---
+++ dummy-crate/target/doc/implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hamt'] = ["impl&lt;K, V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='enum' href='hamt/enum.RcTrick.html' title='hamt::RcTrick'>RcTrick</a>&lt;K, V&gt;","impl&lt;K, V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='enum' href='hamt/enum.ArcTrick.html' title='hamt::ArcTrick'>ArcTrick</a>&lt;K, V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rustdoc/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hamt'] = ["impl&lt;K, V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='enum' href='hamt/enum.Hamt.html' title='hamt::Hamt'>Hamt</a>&lt;K, V, <a class='enum' href='hamt/enum.RcTrick.html' title='hamt::RcTrick'>RcTrick</a>&lt;K, V&gt;&gt;&gt; for <a class='enum' href='hamt/enum.RcTrick.html' title='hamt::RcTrick'>RcTrick</a>&lt;K, V&gt;","impl&lt;K, V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='enum' href='hamt/enum.Hamt.html' title='hamt::Hamt'>Hamt</a>&lt;K, V, <a class='enum' href='hamt/enum.ArcTrick.html' title='hamt::ArcTrick'>ArcTrick</a>&lt;K, V&gt;&gt;&gt; for <a class='enum' href='hamt/enum.ArcTrick.html' title='hamt::ArcTrick'>ArcTrick</a>&lt;K, V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
